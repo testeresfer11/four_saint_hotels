30375ffa4e5aa74eb6b7a25a0fed1131a0833d2c
developer <developer@esfersoft.com>
2025-05-14 18:00:44 +0530

    updates

--- app/Http/Controllers/Api/SabeeHotelController.php
+++ app/Http/Controllers/Api/SabeeHotelController.php
@@ -21,16 +21,52 @@ class SabeeHotelController extends Controller
         $this->sabeeHotelService = $sabeeHotelService;
     }
     /**
-     * functionName : index
+     * functionName : fetchAndStore
      * createdDate  : 23-04-2025
      * purpose      : Get hotel data from sabee and save in our db
+     * /**
+     * Retrieve and store the hotel from sabee app.
+     *
+     * This method accepts a request, processes it through the service layer, 
+     * and returns a JSON response containing the details of the requested page.
+     * It handles exceptions gracefully and returns an appropriate error message in case of failure.
+     * @return \Illuminate\Http\JsonResponse A JSON response containing the hotel listing or an error message.
+     *
+     * @throws \Exception If an error occurs during the retrieval of hotel listing, an exception is thrown 
+     * and an error response is returned to the client.
      */
-
+     
 
     public function fetchAndStore(SabeeHotelService $sabeeHotelService){
         try {
             $hotels = $sabeeHotelService->fetchAndStoreHotels();
             return $this->apiResponse('success', 200, 'Hotel ' . config('constants.SUCCESS.FETCH_DONE'), ['hotels' => $hotels]);
+        } catch (\Exception $e) {
+            return $this->apiResponse('error', 400, $e->getMessage());
+        }
+    }
+
+     /**
+     * functionName : detail
+     * createdDate  : 23-04-2025
+     * purpose      : Get hotel data from sabee and save in our db
+     * 
+     * retriveve hotel details from database.
+     *
+     * This method accepts a request, processes it through the service layer, 
+     * and returns a JSON response containing the details of the requested page.
+     * It handles exceptions gracefully and returns an appropriate error message in case of failure.
+     * @return \Illuminate\Http\JsonResponse A JSON response containing the hotel detail or an error message.
+     *
+     * @throws \Exception If an error occurs during the retrieval of hotel detail, an exception is thrown 
+     * and an error response is returned to the client.
+     */
+     
+
+    public function detail($id){
+        try {
+            $hotel = $sabeeHotelService->hotelDetail($id);
+            return $this->apiResponse('success', 200, 'Hotel ' . config('constants.SUCCESS.FETCH_DONE'), ['hotel' => $hotel]);
         } catch (\Exception $e) {
             return $this->apiResponse('error', 400, $e->getMessage());
         }
