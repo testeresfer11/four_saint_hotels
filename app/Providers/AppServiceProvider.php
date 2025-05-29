fcd0b1b30e3429a6c050fbf034ba0f4d6e3bf768
developer <developer@esfersoft.com>
2025-05-29 15:11:09 +0530

    updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
            View::composer('admin.*', function ($view) {
-            $view->with('hotels', Hotel::select('id', 'name')->get());
+            $view->with('hotels', Hotel::select('id','hotel_id', 'name')->get());
         });
     }
 }
